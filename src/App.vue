9aa0363961d9e0e13dc434443af6dc17ec5ba7bf
IT0163 <nn0979862052@icsc.com.tw>
2023-07-28 08:37:16 +0800

    LV3

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,217 @@
 </script>
 
 <template>
-  <div></div>
+  <!-- one -->
+  <div class="bg">
+    <div class="container header">
+      <div class="header_logo">
+        <img src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/logo-white.png?raw=true"
+          alt="logo">
+      </div>
+      <nav class="nav">
+        <a href="#" class="nav_link">系列鏡框</a>
+        <a href="#" class="nav_link">門市據點</a>
+        <a href="#" class="nav_link">部落格</a>
+        <a href="#" class="nav_link">常見問題</a>
+      </nav>
+    </div>
+  </div>
+  <!-- two -->
+  <div class="bg1">
+    <div class="container">
+      <div class="header_content">
+        <p>Promise-Desert 2020 早春系列</p>
+        <h1>看得清，才能看得遠</h1>
+        <button>立即購買</button>
+      </div>
+    </div>
+  </div>
+  <!-- three -->
+  <div class="bg2">
+    <div class="container txt-center ">
+      <h2>用專業的心，做專業的事</h2>
+      <div class="heart_box">
+        <div class="heart">
+          <div class="heart_img">
+            <img
+              src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-section2-1.png?raw=true"
+              alt="錢">
+          </div>
+          <h3>單一價格</h3>
+          <p>無論任何度數皆不需追加費用即可擁有適合自己的薄型球面鏡片。</p>
+        </div>
+        <div class="heart">
+          <div class="heart_img_2">
+            <img
+              src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-section2-2.png?raw=true"
+              alt="錢">
+          </div>
+          <h3>20 分鐘即可取件</h3>
+          <p>為了您的寶貴時間著想，以豐富專業知識與技術將結帳到交件的時間縮減至最快 20 分鐘即可完成。</p>
+        </div>
+        <div class="heart">
+          <div class="heart_img_3">
+            <img
+              src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-section2-3.png?raw=true"
+              alt="錢">
+          </div>
+          <h3>安心售後服務</h3>
+          <p>我們提供長達 120 天的保固售後服務，不限會員資格皆享有免費深層保養及專業維修服務。</p>
+        </div>
+        <div class="heart">
+          <div class="heart_img_4">
+            <img
+              src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-sectioin2-4.png?raw=true"
+              alt="錢">
+          </div>
+          <h3>關於鏡片</h3>
+          <p>使用世界知名頂級品牌，抗UV、防汙鍍膜薄型非球面鏡片。</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- four -->
+  <div class="container">
+    <h2 class="glass_txt  txt-center">經典系列鏡框</h2>
+    <div class="glass_box">
+      <div class="glass">
+        <div class="glass_img_1"></div>
+        <h3 class="glass_title">OPTICAL</h3>
+      </div>
+      <div class="glass">
+        <div class="glass_img_2"></div>
+        <h3 class="glass_title">SUNGLASSES</h3>
+      </div>
+      <div class="glass">
+        <div class="glass_img_3"></div>
+        <h3 class="glass_title">FUNCTIONAL</h3>
+      </div>
+    </div>
+  </div>
+  <!-- five -->
+  <div class="bg3">
+    <div class="container design">
+      <h3>聯名設計鏡框</h3>
+      <div class="design_box">
+        <div class="design_one">
+          <img src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-section4-1.png?raw=true"
+            alt="人">
+          <div class="design_one_box">
+            <p>DOUBLE A+</p>
+          </div>
+        </div>
+        <div class="design_one">
+          <img src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-section4-2.png?raw=true"
+            alt="眼鏡">
+          <div class="design_one_box">
+            <p>YOUTH</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- six -->
+  <div class="container">
+    <h2 class="custom txt-center">顧客推薦</h2>
+    <div class="custom_cards">
+      <div class="custom_card">
+        <img src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-section5-1.png?raw=true"
+          alt="">
+        <p>Jessy</p>
+        <p>眼鏡品質優良，下次還會想來這邊購買！</p>
+        <p>2021/06/20</p>
+      </div>
+      <div class="custom_card">
+        <img src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-section5-2.png?raw=true"
+          alt="">
+        <p>凱倫</p>
+        <p>做工細緻、鏡架很輕盈，待久也不會覺得有負擔，推薦給大家！</p>
+        <p>2021/04/18</p>
+      </div>
+      <div class="custom_card">
+        <img src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-section5-3.png?raw=true"
+          alt="">
+        <p>悠悠</p>
+        <p>謝謝客服人員的詳細回答，成功買到了喜歡的眼鏡，下次會再回購！</p>
+        <p>2020/12/25</p>
+      </div>
+      <div class="custom_card">
+        <img src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/home-section5-4.png?raw=true"
+          alt="">
+        <p>Kyuan</p>
+        <p>服務很好，品質沒有任何問題，非常喜歡。</p>
+        <p>2020/10/31</p>
+      </div>
+    </div>
+  </div>
+  <!-- seven -->
+  <div class="bg2 ">
+    <div class="contact_box">
+      <h2>聯絡我們</h2>
+      <h3>我們相當重視您的意見，若您有任何疑問，可先參考「常見問題」，若仍有任何問題，請填妥以下資料，我們會在近期與您聯繫。</h3>
+      <p>姓名</p>
+      <input class="input" type="text" placeholder="陳小明">
+      <p>聯絡電話</p>
+      <input class="input" type="text" placeholder="0912-345-678">
+      <p>電子郵件</p>
+      <input class="input" type="text" placeholder="example@email.com">
+      <p>意見反應</p>
+      <input class="input big" type="text" placeholder="請輸入您的意見">
+      <input type="checkbox">
+      <span>我同意隱私權政策，並同意依隱私權政策中所述的方式處理自己的資料。</span>
+      <div class="btn">
+        <a>確認送出</a>
+      </div>
+    </div>
+  </div>
+  <!-- footer -->
+  <div class="bg">
+    <div class="container footer">
+      <div class="footer_one">
+        <div class="info">
+          <ul class="info_ul">
+            <li>首頁</li>
+            <li>系列鏡框</li>
+            <li>門市據點</li>
+            <li>常見問題</li>
+          </ul>
+          <div class="info_img">
+            <img src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/ic-social-fb.png?raw=true" alt="fb">
+            <img src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/ic_social_ig.png?raw=true" alt="ig">
+            <img src="https://github.com/hexschool/2022-web-layout-training/blob/main/week3-4/ic_social_line.png?raw=true" alt="line">
+          </div>
+        </div>
+        <div class="phone ">
+          <i class="fa-solid fa-phone fa-2xl"></i>
+          <span>0800-000-000</span>
+        </div>
+        <div class="mail">
+          <i class="fa-regular fa-envelope fa-2xl"></i>
+          <span>glasses@business.com</span>
+        </div>
+      </div>
+      <div class="footer_two">
+        <p>Copyright © 2020 Glasses.All rights reserved.</p>
+        <div class="footer_three">
+          <p>隱私權政策</p>
+          <p>服務條款</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Bruno+Ace&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;700;900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,900&display=swap');
 
-
-
+@import '@/assets/one';
+@import '@/assets/two';
+@import '@/assets/three';
+@import '@/assets/four';
+@import '@/assets/five';
+@import '@/assets/six';
+@import '@/assets/seven';
+@import '@/assets/footer';
 </style>
